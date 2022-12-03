a9c2da9ee18fc0e8c3bf847df24865ff0a54a6a6
Irineu Antunes <irineu@irineuantunes.com>
2022-12-03 14:39:21 -0300

    initital commit tests

---
+++ HachiNIOApp/HachiNIO.framework/Versions/A/Headers/HachiNIO.h
@@ -0,0 +1,16 @@
+//
+//  HachiNIO.h
+//  HachiNIO
+//
+//  Created by Irineu Antunes on 02/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HachiNIO.
+FOUNDATION_EXPORT double HachiNIOVersionNumber;
+
+//! Project version string for HachiNIO.
+FOUNDATION_EXPORT const unsigned char HachiNIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HachiNIO/PublicHeader.h> 
---
+++ HachiNIOApp/HachiNIOApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HachiNIOApp
+//
+//  Created by Irineu Antunes on 02/12/22.
+//
+
+import Foundation
+import HachiNIO
+
+print("Hello, World!")
+ProtocolServer().sayHello()
